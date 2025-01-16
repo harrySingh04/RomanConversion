a12a708071e9a3c7877ba7d4060f63f200a7262b
Harminder Singh <harmindersingh@Harminders-MBP.hsd1.ca.comcast.net>
2025-01-15 19:31:33 -0800

    Intial project setup for assignment

---
+++ src/main/java/org/adobemarketing/adobeperformance/AdobeperformanceApplication.java
@@ -0,0 +1,13 @@
+package org.adobemarketing.adobeperformance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdobeperformanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdobeperformanceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/adobemarketing/adobeperformance/AdobeperformanceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.adobemarketing.adobeperformance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdobeperformanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
